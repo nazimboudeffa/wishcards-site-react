14082dc5da73e41a8172cb817b323d999da3190c
nazimboudeffa <boudeffa@gmail.com>
2021-08-26 00:15:01 +0200

    Added css grid

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from 'react'; 
+import './Images.css';
+
 import adha from '../assets/images/islam/eid-adha1.png';
 import purnima from '../assets/images/buddhism/buddh-purnima.jpg';
 import dudeism from '../assets/images/dudeism/dudeism-new-cover-image.jpg';
@@ -8,13 +10,15 @@ import achoura from '../assets/images/islam/achoura.jpg';
 class Images extends React.Component {
     render() {
         return (
-            <div className="p-5">
+            <div>
                 <h1>Choose a Card</h1>
-                <a href="adha.html"><img src={adha} alt="adha"/></a>
-                <a href="purnima.html"><img src={purnima} alt=""/></a>
-                <a href="dude.html"><img src={dudeism} alt=""/></a>
-                <a href="newyear.html"><img src={newyear} alt="new year"/></a>
-                <a href="achoura.html"><img src={achoura} alt="achoura"/></a>
+                <div className="wrapper">
+                    <div><a href="adha.html"><img src={adha} alt="adha"/></a></div>
+                    <div><a href="purnima.html"><img src={purnima} alt=""/></a></div>
+                    <div><a href="dude.html"><img src={dudeism} alt=""/></a></div>
+                    <div><a href="newyear.html"><img src={newyear} alt="new year"/></a></div>
+                    <div><a href="achoura.html"><img src={achoura} alt="achoura"/></a></div>
+                </div>
                 <p>My best wishs</p>
                 <p>Nazim</p>
                 <a class="github-fork-ribbon" href="https://github.com/nazimboudeffa/wishcards-site-react" data-ribbon="Fork me on GitHub" title="Fork me on GitHub">Fork me on GitHub</a>
