676ae0cf2eca5755be7e5516f925717818ccf869
nazimboudeffa <boudeffa@gmail.com>
2021-08-26 22:33:18 +0200

    Added grid responsive

--- src/components/Images.js
+++ src/components/Images.js
@@ -13,15 +13,15 @@ class Images extends React.Component {
             <div>
                 <h1>Choose a Card</h1>
                 <div className="wrapper">
-                    <div><a href="adha.html"><img src={adha} alt="adha"/></a></div>
-                    <div><a href="purnima.html"><img src={purnima} alt=""/></a></div>
-                    <div><a href="dude.html"><img src={dudeism} alt=""/></a></div>
-                    <div><a href="newyear.html"><img src={newyear} alt="new year"/></a></div>
-                    <div><a href="achoura.html"><img src={achoura} alt="achoura"/></a></div>
+                    <div className="image"><a href="adha.html"><img src={adha} alt="adha"/></a></div>
+                    <div className="image"><a href="purnima.html"><img src={purnima} alt=""/></a></div>
+                    <div className="image"><a href="dude.html"><img src={dudeism} alt=""/></a></div>
+                    <div className="image"><a href="newyear.html"><img src={newyear} alt="new year"/></a></div>
+                    <div className="image"><a href="achoura.html"><img src={achoura} alt="achoura"/></a></div>
                 </div>
+                <p>You can fork this project on <a href="https://github.com/nazimboudeffa/wishcards-site-react">GitHub</a></p>
                 <p>My best wishs</p>
                 <p>Nazim</p>
-                <a class="github-fork-ribbon" href="https://github.com/nazimboudeffa/wishcards-site-react" data-ribbon="Fork me on GitHub" title="Fork me on GitHub">Fork me on GitHub</a>
             </div>
         )
     }
